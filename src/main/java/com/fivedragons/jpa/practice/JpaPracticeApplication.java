b5fcce262be33b30c9e866763c6444ea27e28e1c
fad <ojy9406@naver.com>
2025-06-23 17:18:45 +0900

    feat. Member 생성, 조회 기능 구현

--- src/main/java/com/fivedragons/jpa/practice/JpaPracticeApplication.java
+++ src/main/java/com/fivedragons/jpa/practice/JpaPracticeApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaPracticeApplication {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("Hello, World!");
-		System.out.println(hello.getData());
 		SpringApplication.run(JpaPracticeApplication.class, args);
 	}
 }
---
+++ src/main/java/com/fivedragons/jpa/practice/service/MemberService.java
@@ -0,0 +1,50 @@
+package com.fivedragons.jpa.practice.service;
+
+import com.fivedragons.jpa.practice.domain.Member;
+import com.fivedragons.jpa.practice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     *  - 중복 회원 검증
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한건 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.find(memberId);
+    }
+}

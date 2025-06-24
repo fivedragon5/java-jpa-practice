a2eec7814d76854bc3b07ea3ab4c1ba2a1bbab44
fad <ojy9406@naver.com>
2025-06-24 14:05:28 +0900

    feat. 주문, 주문 상품 엔티티 개발

--- src/main/java/com/fivedragons/jpa/practice/domain/Order.java
+++ src/main/java/com/fivedragons/jpa/practice/domain/Order.java
@@ -42,6 +42,7 @@ public class Order {
 
     private OrderStatus status;
 
+    // 연관 관계 메서드
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
@@ -55,5 +56,45 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createORder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // == 비즈니스 로직 ==
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        // 이미 배송 완료인 경우
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // == 조회 로직 ==
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
     }
 }

55c43ad410ae96fb68cecd6a0eac33398ad27e88
fad <ojy9406@naver.com>
2025-06-24 18:40:18 +0900

    feat. 주문 목록 검색, 주문 취소 기능 개발

--- src/main/java/com/fivedragons/jpa/practice/controller/OrderController.java
+++ src/main/java/com/fivedragons/jpa/practice/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.fivedragons.jpa.practice.controller;
 
 import com.fivedragons.jpa.practice.domain.Member;
+import com.fivedragons.jpa.practice.domain.Order;
+import com.fivedragons.jpa.practice.domain.OrderSearch;
 import com.fivedragons.jpa.practice.domain.item.Item;
 import com.fivedragons.jpa.practice.service.ItemService;
 import com.fivedragons.jpa.practice.service.MemberService;
@@ -9,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,6 +43,19 @@ public class OrderController {
                         @RequestParam("count") int count
     ) {
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
--- src/main/java/com/fivedragons/jpa/practice/domain/Order.java
+++ src/main/java/com/fivedragons/jpa/practice/domain/Order.java
@@ -3,6 +3,8 @@ package com.fivedragons.jpa.practice.domain;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -42,6 +44,8 @@ public class Order {
     private Delivery delivery;
 
     private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
     // 연관 관계 메서드

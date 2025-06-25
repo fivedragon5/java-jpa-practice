b672b637e20902ccf440a4784a1520643ba9ec9b
fad <ojy9406@naver.com>
2025-06-25 17:06:45 +0900

    feat. 회원 수정 API 추가

--- src/main/java/com/fivedragons/jpa/practice/api/MemberApiController.java
+++ src/main/java/com/fivedragons/jpa/practice/api/MemberApiController.java
@@ -5,9 +5,12 @@ import com.fivedragons.jpa.practice.domain.Member;
 import com.fivedragons.jpa.practice.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,19 +20,45 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/v1/members")
-    public createMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
-        return new createMemberResponse(id);
+        return new CreateMemberResponse(id);
     }
 
     @PostMapping("/api/v2/members")
-    public createMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Long id = memberService.join(request.toMember());
-        return new createMemberResponse(id);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable Long id,
+            @RequestBody @Valid UpdateMemberRequest request
+    ) {
+        memberService.update(id, request.getName());
+        Member updatedMember = memberService.findOne(id);
+        return new UpdateMemberResponse(updatedMember.getId(), updatedMember.getName());
     }
 
     @Data
-    static class CreateMemberRequest {
+    public static class CreateMemberRequest {
+        @NotEmpty
+        private final String name;
+        private final String city;
+        private final String street;
+        private final String zipcode;
+
+        public Member toMember() {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
+        }
+    }
+
+    @Data
+    public static class UpdateMemberRequest {
         @NotEmpty
         private String name;
         private String city;
@@ -45,10 +74,15 @@ public class MemberApiController {
     }
 
     @Data
-    static class createMemberResponse {
+    @AllArgsConstructor
+    public static class CreateMemberResponse {
         private Long id;
-        public createMemberResponse(Long id) {
-            this.id = id;
-        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }

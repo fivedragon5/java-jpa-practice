a86ada6f68badf162b021bf0b1c28627f274b0e1
fad <ojy9406@naver.com>
2025-06-27 16:46:26 +0900

    feat. 컬렉션 조회) 주문 조회 V3 - 엔티티를 DTO로 변환 - 페이징 한계 이슈 해결

--- src/main/java/com/fivedragons/jpa/practice/api/OrderApiController.java
+++ src/main/java/com/fivedragons/jpa/practice/api/OrderApiController.java
@@ -9,6 +9,7 @@ import com.fivedragons.jpa.practice.repository.OrderRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -50,6 +51,17 @@ public class OrderApiController {
         for (Order o : orders) {
             System.out.println("order ref = " + o + "id = " + o.getId());
         }
+        return orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         return orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());

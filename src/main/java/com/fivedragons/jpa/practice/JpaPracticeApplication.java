09cdfde5bf8c61b8a3e8e5c22dbb3c1d46fca8a9
fad <ojy9406@naver.com>
2025-06-27 16:02:00 +0900

    feat. 컬렉션 조회) 주문 조회 V2 - 엔티티를 DTO로 변환

--- src/main/java/com/fivedragons/jpa/practice/JpaPracticeApplication.java
+++ src/main/java/com/fivedragons/jpa/practice/JpaPracticeApplication.java
@@ -1,12 +1,19 @@
 package com.fivedragons.jpa.practice;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaPracticeApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5JakartaModule hibernate5JakartaModule() {
+		return new Hibernate5JakartaModule();
 	}
 }
